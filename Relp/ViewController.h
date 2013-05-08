d5e3555ce60d4d5ca13416e9760d2250285c09dd
Rodrigo Parra <rodrigo@bright.com>
2013-05-07 21:54:47 -0700

    Created default project with custom views

---
+++ Relp/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Relp
+//
+//  Created by Rodrigo Parra on 5/6/13.
+//  Copyright (c) 2013 Rodrigo Parra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) UIPopoverController *createPopover;
+- (IBAction)createButtonPressed:(id)sender;
+
+
+@end
